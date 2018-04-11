cb04a9b0f659eb6e336ec3867f17737839160a74
Mingyang Yan <myyan@bu.edu>
2018-04-11 17:06:08 -0400

    NaviBar

---
+++ NaviBar/app/src/main/java/com/cs591/mooncake/MainActivity.java
@@ -0,0 +1,77 @@
+package com.cs591.mooncake;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout mainFrame;
+    private BottomNavigationView navigation;
+
+    private ExploreFragment exploreFragment;
+    private LikeFragment likeFragment;
+    private ScheduleFragment scheduleFragment;
+    private MapFragment mapFragment;
+    private ProfileFragment profileFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainFrame = (FrameLayout) findViewById(R.id.mainFrame);
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+        exploreFragment = new ExploreFragment();
+        likeFragment = new LikeFragment();
+        scheduleFragment = new ScheduleFragment();
+        mapFragment = new MapFragment();
+        profileFragment = new ProfileFragment();
+
+        setFragment(exploreFragment);
+
+        NaviBarHelper.disableShiftMode(navigation);
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.navigation_explore:
+                        setFragment(exploreFragment);
+                        return true;
+                    case R.id.navigation_like:
+                        setFragment(likeFragment);
+                        return true;
+                    case R.id.navigation_schedule:
+                        setFragment(scheduleFragment);
+                        return true;
+                    case R.id.navigation_map:
+                        setFragment(mapFragment);
+                        return true;
+                    case R.id.navigation_profile:
+                        setFragment(profileFragment);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainFrame, fragment);
+        fragmentTransaction.commit();
+
+    }
+
+}
---
+++ NaviBar/app/src/main/java/com/cs591/mooncake/NaviBarHelper.java
@@ -0,0 +1,36 @@
+package com.cs591.mooncake;
+
+/**
+ * Created by yanmingyang on 4/11/18.
+ * Ref: https://stackoverflow.com/questions/41649494/how-to-remove-icon-animation-for-bottom-navigation-view-in-android?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+ */
+
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+import java.lang.reflect.Field;
+
+public class NaviBarHelper {
+    public static void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                //noinspection RestrictedApi
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BNVHelper", "Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BNVHelper", "Unable to change value of shift mode", e);
+        }
+    }
+}
