805b9fccd6084d1a03ee019b303f2c8b25974c10
allenyzs <yzsallen@gmail.com>
2018-04-22 20:15:47 -0400

    map update

--- NaviBar/app/src/main/java/com/cs591/mooncake/map/ShowInfo.java
+++ NaviBar/app/src/main/java/com/cs591/mooncake/map/ShowInfo.java
@@ -3,6 +3,8 @@ package com.cs591.mooncake.map;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.style.BulletSpan;
+import android.util.Log;
 
 import com.cs591.mooncake.R;
 
@@ -11,9 +13,43 @@ import com.cs591.mooncake.R;
  */
 
 public class ShowInfo extends AppCompatActivity{
+    public final String TAG = "SUP";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_map_gsu_2ndfloor);
+        Bundle extras = getIntent().getExtras();
+        String result = extras.getString("location");
+        Log.w(TAG,result);
+
+        if (result.equals("BU_GSU")){
+            setContentView(R.layout.fragment_map_gsu_2ndfloor);
+        } else if (result.equals("BU_Tsai")){
+            setContentView(R.layout.fragment_map_tsai);
+        }
+
+
+
+
+//        String locaTitle;
+//
+//        if (savedInstanceState == null){
+//
+//            Bundle extras = getIntent().getExtras();
+//            if (extras == null){
+//                locaTitle = null;
+//            } else {
+//                locaTitle = extras.getString("location");
+//            }
+//        } else {
+//            locaTitle = (String) savedInstanceState.getSerializable("location");
+//
+//            if (locaTitle.equals("BU_GSU")){
+//                setContentView(R.layout.fragment_map_gsu_2ndfloor);
+//            } else if (locaTitle.equals("BU_Tsai")){
+//                setContentView(R.layout.fragment_map_tsai);
+//            }
+//        }
+//
     }
+
 }
