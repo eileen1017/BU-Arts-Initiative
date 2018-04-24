9598d6caaac76e50623dbb5c517a33220985b679
QifanHe <heqifan0109@gmail.com>
2018-04-23 22:16:45 -0400

    Big Huge update before 4/24

--- NaviBar/app/src/main/java/com/cs591/mooncake/explore/EventActivity.java
+++ NaviBar/app/src/main/java/com/cs591/mooncake/explore/EventActivity.java
@@ -1,15 +1,59 @@
 package com.cs591.mooncake.explore;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 import com.cs591.mooncake.R;
+import com.cs591.mooncake.SQLite.MySQLiteHelper;
+import com.cs591.mooncake.SQLite.SingleEvent;
 
 public class EventActivity extends AppCompatActivity {
 
+    MySQLiteHelper myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        myDb = new MySQLiteHelper(this);
+
+        Intent extras = getIntent();
+        Bundle bundle = extras.getExtras();
+        int eventID = bundle.getInt("eventID");
+        populateUI(myDb.getEvent(eventID));
     }
+
+    private void populateUI(SingleEvent singleEvent) {
+        ImageView ivEvent = findViewById(R.id.ivEvent);
+        TextView tvEventType = findViewById(R.id.tvEventType);
+        TextView tvEventTime = findViewById(R.id.tvEventTime);
+        TextView tvEventArtist = findViewById(R.id.tvEventArtist);
+        Toolbar tbEventTitle = findViewById(R.id.tbEventTitle);
+
+        tbEventTitle.setTitle(singleEvent.getName());
+        tvEventType.setText(singleEvent.getType());
+        ivEvent.setImageBitmap(singleEvent.getPic());
+        ivEvent.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        String date;
+        if (singleEvent.getDate() == 5) {
+            date = "10/5 Fri. ";
+        } else {
+            date = "10/6 Sat. ";
+        }
+
+        tvEventTime.setText(date + singleEvent.getStart());
+        tvEventArtist.setText(singleEvent.getArtist());
+
+    }
+
+
+
+
+
+
 }
--- NaviBar/app/src/main/java/com/cs591/mooncake/profile/ProfileFragment.java
+++ NaviBar/app/src/main/java/com/cs591/mooncake/profile/ProfileFragment.java
@@ -11,7 +11,13 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
+import com.cs591.mooncake.MainActivity;
 import com.cs591.mooncake.R;
+import com.cs591.mooncake.SQLite.MySQLiteHelper;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.twitter.sdk.android.core.TwitterCore;
 
 import java.util.ArrayList;
 
@@ -47,13 +53,23 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         mListView = (ListView) view.findViewById(R.id.item_menu);
-        CustomAdapter customAdapter = new CustomAdapter(getContext(), Icons, Names);
+        CustomAdapter customAdapter = new CustomAdapter(getActivity(), Icons, Names);
         mListView.setAdapter(customAdapter);
+
+        view.findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MySQLiteHelper(getActivity()).initProfile();
+                FirebaseAuth.getInstance().signOut();
+                LoginManager.getInstance().logOut();
+                TwitterCore.getInstance().getSessionManager().clearActiveSession();
+            }
+        });
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(getContext(),classes[position]);
+                Intent i = new Intent(getActivity(),classes[position]);
                 startActivity(i);
 
             }
