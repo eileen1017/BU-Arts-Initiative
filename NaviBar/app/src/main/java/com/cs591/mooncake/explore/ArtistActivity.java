69ce13e8b6927b69b50b8013fc414259fe9841b8
Mingyang Yan <myyan@bu.edu>
2018-04-26 11:23:50 -0400

    update

--- NaviBar/app/src/main/java/com/cs591/mooncake/explore/ArtistActivity.java
+++ NaviBar/app/src/main/java/com/cs591/mooncake/explore/ArtistActivity.java
@@ -2,6 +2,7 @@ package com.cs591.mooncake.explore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.cs591.mooncake.R;
 
 public class ArtistActivity extends AppCompatActivity {
 
